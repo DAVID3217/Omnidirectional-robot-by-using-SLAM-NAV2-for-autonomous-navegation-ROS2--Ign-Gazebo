c3a75fa8b2366978427cfa0307b5d5fff611175a
David Santiago <davidsanty27@gmail.com>
2025-10-03 14:37:42 +0300

    actualizacion1

--- launch/one_robot_gz_launch.py
+++ launch/one_robot_gz_launch.py
@@ -1,28 +1,31 @@
-import os
-import xacro
+import os, xacro
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.substitutions import Command
 
-robot_model = 'omnibot2'
-robot_ns = 'r1'
+robot_model = 'omnibot1.sdf'
+robot_model_2 = 'omnibot2' #caster_diffbot (bug when V > 0.35), usabase, omnibot
+robot_ns = 'r1' # Robot namespace (robot name)
 pose = ['1.0', '0.0', '0.0', '0.0'] #Initial robot pose: x,y,z,th
-robot_base_color = '0.0 0.0 1.0 0.95'
-world_file = 'warehouse.sdf'
+robot_base_color = '0.0 0.0 1.0 0.98' #Ign and Rviz color of the robot's main body (rgba)
+world_file = 'warehouse' # empty, building_map
 
 def generate_launch_description():
+    package_name = 'ign_robots_examples'
 
-    this_pkg_path = os.path.join(get_package_share_directory('ign_robots_examples'))
+    this_pkg_path = os.path.join(get_package_share_directory(package_name))
+    # ~/colcon_ws/install/rp_nav2/share/rp_nav2/
 
     simu_time = DeclareLaunchArgument(
         'use_sim_time',
         default_value='True',
         description='Use simulation (Gazebo) clock if true')
-
-
+    
+    # Set ign sim resource path
     ign_resource_path = SetEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
         value=[
@@ -35,25 +38,27 @@ def generate_launch_description():
         executable='rviz2',
         name='rviz2',
         output='screen',
-        arguments=['-d', os.path.join(this_pkg_path, 'rviz', 'one_robot.rviz')],
+        arguments=['-d', str(this_pkg_path+"/rviz/one_robot.rviz")],
     )
 
     open_ign = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
-        ]),
-        launch_arguments=[
-            ('gz_args', [os.path.join(this_pkg_path, 'worlds', world_file), ' -v 4', ' -r'])
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+            launch_arguments=[
+                ('gz_args', [this_pkg_path+"/worlds/"+world_file+".sdf", ' -v 4', ' -r'])
+
         ]
     )
+    
+    xacro_file = os.path.join(this_pkg_path, 'urdf', robot_model_2+'.urdf') #.urdf
 
-    xacro_file = os.path.join(this_pkg_path, 'urdf', robot_model + '.xacro')
-    doc = xacro.process_file(
-        xacro_file,
-        mappings={'base_color': robot_base_color}
-    )
-    robot_desc = doc.toprettyxml(indent='  ')
+    doc = xacro.process_file(xacro_file,
+        mappings={'base_color' : robot_base_color})
 
+    robot_desc = doc.toxml()
+
+    sdf_file = os.path.join(this_pkg_path, 'sdf', robot_model)
+    
     gz_spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
@@ -66,26 +71,15 @@ def generate_launch_description():
     )
 
     robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        #namespace=robot_ns,
-        output='screen',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        # namespace=robot_ns,
+        output="screen",
         parameters=[{'robot_description': robot_desc}]
     )
 
-    '''nav2_launch = IncludeLaunchDescription(
-    PythonLaunchDescriptionSource(
-        os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'slam_launch.py')
-    ),
-    launch_arguments={
-        'use_sim_time': 'True',
-        'params_file': os.path.join(this_pkg_path, 'config', 'nav2_params.yaml'),
-        'slam': 'True'
-    }.items()
-    )'''''
-
-# Bridge
+    # Bridge
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -110,15 +104,24 @@ def generate_launch_description():
             ('/world/world_model/model/'+robot_ns+'/joint_state', '/joint_states'),
         ]
     )
-    
 
-    return LaunchDescription([
-        simu_time,
-        ign_resource_path,
-        open_rviz,
-        open_ign,
-        gz_spawn_entity,
-        robot_state_publisher,
-        bridge
-        #nav2_launch
-    ])
+    tf_broadcaster_odom = Node(
+        package=package_name,
+        executable="tf_broadcaster",
+        output="screen",
+        arguments=[robot_ns]
+    )
+
+
+    return LaunchDescription(
+        [
+            simu_time,
+            ign_resource_path,
+            open_ign,
+            gz_spawn_entity,
+            robot_state_publisher,
+            bridge,
+            tf_broadcaster_odom,
+            open_rviz
+        ]
+    )
